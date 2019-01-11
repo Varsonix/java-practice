4bf45c320492a6c17b4b6b22f56411cbf73e462e
Jordan Gibbard <46365880+Varsonix@users.noreply.github.com>
2019-01-10 23:46:29 -0800

    Update variable-types.java

--- variable-types.java
+++ variable-types.java
@@ -7,3 +7,21 @@ public class Variables {
 
 	}
 }
+
+// Learning arithmatic operators.
+public class Modulo {
+	public static void main(String[] args) {
+
+		int myRemainder = 14 % 6;
+		int myNumber = 2 + 2;
+		int myProduct = 4 * 8;
+		int myDivision = 8 / 4;
+		int mySubtraction = 9 - 3;
+		System.out.println(myRemainder);
+		System.out.println(myNumber);
+		System.out.println(myProduct);
+		System.out.println(myDivision);
+		System.out.println(mySubtraction);
+
+	}
+} 
